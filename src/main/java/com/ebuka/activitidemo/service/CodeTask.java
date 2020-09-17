589bdf7149f92f50352131accb20f33096a7b38a
phayo <anazodogodwin@gmail.com>
2020-09-17 08:57:52 +0100

    Add bpmn20 xml

---
+++ src/main/java/com/ebuka/activitidemo/service/CodeTask.java
@@ -0,0 +1,10 @@
+package com.ebuka.activitidemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CodeTask {
+    public boolean creditSeller(String sellerId){
+        return true;
+    }
+}
